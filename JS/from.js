5d2095617b8aa963319871abb4029173df43f0c0
Dipanjan59173 <68274748+Dipanjan59173@users.noreply.github.com>
2020-07-27 23:54:37 +0530

    Update from.js

--- JS/from.js
+++ JS/from.js
@@ -1,37 +1,60 @@
-'use strict';
+// 'use strict';
 let btn = document.getElementById('btnsubmit')
 
 // let email = document.querySelector('#btn')
-// let  div1 = document.querySelectorAll('.myclass')
+
 // let div=document.getElementsByClassName('myclass')[1]
 function onBtnClick(){
     let userName = document.getElementById('uname').value
-let email = document.getElementById('email').value
-let pass = document.getElementById('pass').value
-let cpass = document.getElementById('cpass').value
-    if(userName.value=='NULL' || userName.value.length<4){
-        div.innerHTML='Username must be3 atleast 3 characters'
+    if(userName.value=='null' || userName.length<3 || userName.value==""){
+        document.getElementById('name_e').innerHTML='Username must be atleast 3 characters';
     }
-    let mailformat = /^w+([.-]?w+)@w+([.-]?w+)(.w{2,3})+$/;
-    if(email!=null)
+    else
     {
-        if(email.value.match(mailformat))
+        document.getElementById('name_e').innerHTML='';
+    }  
+    
+    //-----for email------ 
+    let email = document.getElementById('email').value
+    let emailCheck=ValidateEmail(email);
+    
+    if(emailCheck!= true)
+    {
+        document.getElementById('err_email').innerHTML= 'Email is not valid';
+    }
+    else
+    {
+        document.getElementById('err_email').innerHTML='';
+    } 
+        
+    
+        //-------Password------
+    let pass = document.getElementById('pass').value
+    let cpass = document.getElementById('cpass').value
+     if(pass==null || pass.length<7){
+        
+        document.getElementById('e-pass').innerHTML='Password must be atleast 6 characters';
+    }
+    if(cpass!=pass || cpass==null){
+        document.getElementById('e_c').innerHTML='confrimed password  is required';
+    }
+    else if (cpass!=pass)
         {
+            document.getElementById('e_c').innerHTML='Password not matched with confirmed password';          
+         }
+    
+    
+}
+
+
+function ValidateEmail(email) 
+        {
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        if (reg.test(email) == false) 
+        {        
+        return false;
+        }
         return true;
         }
-        else{
-            div.innerHTML= 'Email is not valid'
 
-        }
-    }
-    if(pass==null || pass.length<7){
-        div.innerHTML='Password must be atleast 6 characters'
-    }
-    if(cpass!==pass || cpass===null){
-        div.innerHTML='password2 is required'
-    }
-    if(userName=='NULL' || userName.length<4){
-        div.innerHTML='Username must be3 atleast 3 characters'
-    }
-}
-btnsubmit.addEventListener('click',onBtnClick)
+//btn.addEventListener('click',validateForm)
